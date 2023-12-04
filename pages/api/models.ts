5d6f9c99fb9ce50c33f396597a9fe6c356f764bb
dp262 <huangwudong2011@gmail.com>
2023-12-05 07:51:02 +0800

    Update models.ts

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -1,5 +1,4 @@
 import { OPENAI_API_HOST, OPENAI_API_TYPE, OPENAI_API_VERSION, OPENAI_ORGANIZATION } from '@/utils/app/const';
-
 import { OpenAIModel, OpenAIModelID, OpenAIModels } from '@/types/openai';
 
 export const config = {
@@ -32,12 +31,8 @@ const handler = async (req: Request): Promise<Response> => {
       },
     });
 
-    if (response.status === 401) {
-      return new Response(response.body, {
-        status: 500,
-        headers: response.headers,
-      });
-    } else if (response.status !== 200) {
+    // 处理 API 响应
+    if (response.status !== 200) {
       console.error(
         `OpenAI API returned an error ${
           response.status
@@ -62,6 +57,7 @@ const handler = async (req: Request): Promise<Response> => {
       })
       .filter(Boolean);
 
+    // 返回处理后的响应
     return new Response(JSON.stringify(models), { status: 200 });
   } catch (error) {
     console.error(error);
