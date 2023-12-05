9370ae063c04ea606462ceeb860d9ea5f9968a09
dp262 <huangwudong2011@gmail.com>
2023-12-05 08:04:39 +0800

    Update errorService.ts

--- services/errorService.ts
+++ services/errorService.ts
@@ -8,27 +8,7 @@ const useErrorService = () => {
   const { t } = useTranslation('chat');
 
   return {
-    getModelsError: useMemo(
-      () => (error: any) => {
-        return !error
-          ? null
-          : ({
-              title: t('Error fetching models.'),
-              code: error.status || 'unknown',
-              messageLines: error.statusText
-                ? [error.statusText]
-                : [
-                    t(
-                      'Make sure your OpenAI API key is set in the bottom left of the sidebar.',
-                    ),
-                    t(
-                      'If you completed this step, OpenAI may be experiencing issues.',
-                    ),
-                  ],
-            } as ErrorMessage);
-      },
-      [t],
-    ),
+    getModelsError: useMemo(() => (error: any) => null, []), // Always return null
   };
 };
 
